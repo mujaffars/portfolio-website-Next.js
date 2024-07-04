fbc80a3f3f97a4d82899a107da891459bbe00bf0
mujaffars <mujaffar.sanadi01@gmail.com>
2024-07-04 13:19:41 +0530

    Created about, contact, projects pages

---
+++ pages/contact.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Contact() {
+    return (
+        <div>
+            <Head>
+                <title>Contact Me</title>
+            </Head>
+            <main>
+                <h1>Contact Me</h1>
+                <p>You can reach me at [Your Email]...</p>
+            </main>
+        </div>
+    );
+}
---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Projects() {
+    return (
+        <div>
+            <Head>
+                <title>My Projects</title>
+            </Head>
+            <main>
+                <h1>My Projects</h1>
+                <p>Here are some of the projects I've worked on...</p>
+            </main>
+        </div>
+    );
+}
